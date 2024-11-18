8814222f6b53424154c11919ffaf207c4667eb68
MCCLab4804WD23 <ianmclaughlin115@gmail.com>
2024-11-18 14:50:56 -0800

    Tax Evaders type 1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module edu.miracosta.cs112.finalproject.finalproject {
 
     opens edu.miracosta.cs112.finalproject.finalproject to javafx.fxml;
     exports edu.miracosta.cs112.finalproject.finalproject;
+    exports edu.miracosta.cs112.finalproject.finalproject.controllers;
+    opens edu.miracosta.cs112.finalproject.finalproject.controllers to javafx.fxml;
 }
