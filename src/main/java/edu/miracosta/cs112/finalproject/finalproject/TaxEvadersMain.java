0f8de399bc5a859fab5bbf0de39efcf2ff0d0c18
MCCLab4804WD23 <ianmclaughlin115@gmail.com>
2024-11-20 14:52:49 -0800

    Fix the button brotha.

---
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/TaxEvadersMain.java
@@ -0,0 +1,24 @@
+package edu.miracosta.cs112.finalproject.finalproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TaxEvadersMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Tax Evaders!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/menuController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/menuController.java
@@ -1,4 +1,15 @@
 package edu.miracosta.cs112.finalproject.finalproject.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class menuController {
+    @FXML
+    ImageView radImage;
+    @FXML
+    Label titleText;
+    @FXML
+    Button startButton;
 }
