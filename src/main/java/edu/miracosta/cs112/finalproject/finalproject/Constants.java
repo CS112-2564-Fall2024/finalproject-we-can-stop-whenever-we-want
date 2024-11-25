583dd979cf26f02f810f42f4ef84651951ee851c
MCCLab4804WD23 <ianmclaughlin115@gmail.com>
2024-11-25 14:35:45 -0800

    No Monni??

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Constants.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String SCORE_TEXT = "NOOB";
     public static final String MAIN_TITLE_TEXT = "Finale Project";
     public static final String GAME_TITLE_TEXT = "Play Please";
-    public static final String RAD_IMAGE_PATH = "file: ./src/main/resources/Images/nomoni.png";
+    public static final String RAD_IMAGE_PATH = "file:./src/main/resources/Images/nomoni.png";
 
 
 }
--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/gameController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/gameController.java
@@ -3,15 +3,32 @@ package edu.miracosta.cs112.finalproject.finalproject.controllers;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.awt.*;
+
+import static edu.miracosta.cs112.finalproject.finalproject.Constants.RAD_IMAGE_PATH;
 import static edu.miracosta.cs112.finalproject.finalproject.Constants.SCORE_TEXT;
 
 public class gameController {
     @FXML
     Label scoreGoesHere;
+    @FXML
+    ImageView lifeOne;
+    @FXML
+    ImageView lifeTwo;
+    @FXML
+    ImageView lifeThree;
+//    @FXML
+//    Canvas gameBoard;
 
     public void initialize(){
-        scoreGoesHere.setText(SCORE_TEXT); //error here saying string cannot be converted to long
+        scoreGoesHere.setText(SCORE_TEXT);
+        Image image = new Image(RAD_IMAGE_PATH); // Ensure RAD_IMAGE_PATH is correct
+        lifeOne.setImage(image);
+        lifeTwo.setImage(image);
+        lifeThree.setImage(image);
     }
 
 
