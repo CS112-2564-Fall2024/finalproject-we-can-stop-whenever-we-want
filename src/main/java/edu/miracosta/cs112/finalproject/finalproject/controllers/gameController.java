ca7918d80e736ed9ba808751cef26d0e4b4978db
MCCLab4804WD23 <ianmclaughlin115@gmail.com>
2024-11-27 14:20:22 -0800

    Working background and green circle code.

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/gameController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/gameController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.Canvas; // Used ai to fix the canvas import
 import javafx.scene.paint.Color;
 
 import java.util.Random;
@@ -27,7 +27,7 @@ public class gameController {
     @FXML
     ImageView lifeThree;
     @FXML
-    private Canvas canvas; // Make sure this is injected correctly
+    private Canvas canvas; // Used ai to fix the canvas fxml
 
     private GraphicsContext gc;
 
@@ -38,22 +38,30 @@ public class gameController {
     Random random = new Random();
     public void initialize(){
         scoreGoesHere.setText(SCORE_TEXT);
-        Image image = new Image(RAD_IMAGE_PATH); // Ensure RAD_IMAGE_PATH is correct
+        Image image = new Image(RAD_IMAGE_PATH);
         lifeOne.setImage(image);
         lifeTwo.setImage(image);
         lifeThree.setImage(image);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        deltaX = 0;
+
+        deltaX = random.nextDouble(600);
         deltaY = 1;
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                gc.setFill(Color.BLACK);
+                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+                handleObjects();
+
+            }
+            public void handleObjects(){
                 gc.setFill(Color.GREEN);
-                gc.fillOval(drawX + 200, drawY, 100, 100);
-                drawX += deltaX;
+                gc.fillOval(deltaX, drawY - 200, 100, 100);
+//                drawX += deltaX;
                 drawY += deltaY;
             }
         };
